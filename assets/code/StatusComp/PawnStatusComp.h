0dd0225f8db57d91cc6fa0a4fe1ce9e1d521ed3e
Devin <ealydevin413@gmail.com>
2021-02-02 13:18:48 +0100

    Added missing enums

--- assets/code/StatusComp/PawnStatusComp.h
+++ assets/code/StatusComp/PawnStatusComp.h
@@ -10,10 +10,126 @@
 #include "Core.h"
 #include "Net/UnrealNetwork.h"
 
-//Epi
-#include "Epidemic/Game/EpiFunctionLibrary.h"
 #include "PawnStatusComp.generated.h"
 
+// STATUS SYSTEM
+
+UENUM(BlueprintType)
+enum EStatus
+{
+	//Delta Describes the Increase or Decrease of a Value.
+	Health        UMETA(DisplayName = "Health"),
+    Blood        UMETA(DisplayName = "Blood"), 
+    Water         UMETA(DisplayName = "Water"),    
+    Food          UMETA(DisplayName = "Food"),   
+    Heat          UMETA(DisplayName = "Heat"),
+	Stamina          UMETA(DisplayName = "Stamina"),
+    //Keep this the last entry so we know the lenght.
+    LastEntry          UMETA(DisplayName = "LastEntry")
+};    
+
+UENUM(BlueprintType)
+enum EStatusInfo
+{
+	Base         UMETA(DisplayName = "Base"),  
+    Max          UMETA(DisplayName = "Max"),
+    Min          UMETA(DisplayName = "Min")
+};  
+
+UENUM(BlueprintType)
+enum EStatusMeta
+{
+	StatusBase         UMETA(DisplayName = "StatusBase"),  
+    StatusDelta          UMETA(DisplayName = "StatusDelta")
+}; 
+
+USTRUCT(BlueprintType)
+struct FStatusBase
+{
+	GENERATED_USTRUCT_BODY()
+    
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	float Base;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	float Min;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	float Max;
+};
+
+USTRUCT(BlueprintType)
+struct FStatusEffect
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	FString StatusName;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	TEnumAsByte<EStatus> StatusToEffect;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	float Duration;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	float Amount;
+
+	FStatusEffect()
+	{
+		StatusName = "none";
+		StatusToEffect = LastEntry;
+		Duration = 0.0f;
+		Amount = 0.0f;	
+	};
+
+	FStatusEffect(EStatus NewStatus, float NewDuration, float NewAmount)
+	{
+		StatusName = "none";
+		StatusToEffect = NewStatus;
+		Duration = NewDuration;
+		Amount = NewAmount;	
+	};
+};
+
+USTRUCT(BlueprintType)
+struct FStatusFull
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	TEnumAsByte<EStatus> Status;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	FStatusBase StatusBase;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	FStatusBase StatusDelta;
+};
+
+USTRUCT(BlueprintType)
+struct FEffectValues
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	float Duration;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	float Amount;
+
+	FEffectValues()
+	{
+		Duration = 1.0f;
+		Amount = 1.0f;
+	};
+
+	FEffectValues(float NewDuration, float NewAmount)
+	{
+		Duration = NewDuration;
+		Amount = NewAmount;
+	};
+};
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnStatusChanged, EStatus, Name);
 
